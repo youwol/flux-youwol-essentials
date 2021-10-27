5e94c38de0c686c0571036749737004867c2b684
greinisch <greinisch@youwol.com>
2021-10-27 10:54:42 +0200

    :art: factorize idex.ts

---
+++ src/lib/index.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of @youwol/flux-code-mirror
+ */
+export * from './main';
+export * from './drive.module';
+export * from './drive';
+export * from './assets-gateway-client';
